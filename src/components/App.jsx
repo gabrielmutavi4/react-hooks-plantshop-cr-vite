f812bc94e95d0bbff653a63cb8b88adb84be088f
gabrielmutavi4-GM <gabrielmutavi4@gmail.com>
2026-05-11 15:25:02 +0300

    update plant shop files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import Header from "./Header";
-import PlantPage from "./PlantPage";
+import React, { useEffect, useState } from "react";
+import PlantList from "./PlantList";
+import NewPlantForm from "./NewPlantForm";
+import Search from "./Search";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  function addPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="app">
-      <Header />
-      <PlantPage />
+    <div>
+      <h1>Plant Shop</h1>
+
+      <Search search={search} setSearch={setSearch} />
+
+      <NewPlantForm addPlant={addPlant} />
+
+      <PlantList plants={filteredPlants} />
     </div>
   );
 }
